f306546316fd16d5dc857bbc4dafe7d8fef86f1f
Pavandeep <pavandeepkumarmlk@gmail.com>
2023-12-23 09:19:47 +0530

    multile ternary operater

--- DAY_01/script.js
+++ DAY_01/script.js
@@ -1,1 +1,16 @@
-console.log("Pavandeep umar")
+// const checkForVote = (age) => {
+//     const result=age > 18 ? "you are eligible for vote" : age < 18 ? "Now you are child": age == 18 ? "Your first vote this year" : "";
+//     return result
+// }
+
+// console.log(checkForVote(112))
+
+
+
+
+
+
+
+
+
+
